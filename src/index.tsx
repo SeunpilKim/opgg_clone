593559d4960de45c595a9454b8f8b6c696346fcd
SeunpilKim <seunpilk@gmail.com>
2022-03-25 13:58:49 +0900

    inital commit

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/app.css'
+
+const App = () => {
+  return (
+    <div className="page-wrapper">
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
